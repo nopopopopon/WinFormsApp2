a947e540c9806632c31d720691ca62a2134a354a
nopopopopon <G7Daisym1Git>
2021-12-19 08:33:52 +0900

    ボタン1を追加

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -42,11 +43,21 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "これは最初からDNET5にしたものです";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(309, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1ついか";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -58,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
